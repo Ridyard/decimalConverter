43b27c5592d3bd1fae0610b541127c4ebb45ea20
Alex Ridyard <alexanderridyard@gmail.com>
2021-05-06 00:19:45 +0100

    added binary to dec function

--- decimalConverter.py
+++ decimalConverter.py
@@ -1,30 +1,49 @@
 # misc project
 # convert decimal number into binary
 
-## to do: bin to dec, hex converter
+## to do: add gui, hex converter
 
 import pyinputplus as pyip
 
 
-def binaryConv(num, runningTotal):
-    quotient, rem = divmod(num, 2)
-    print(quotient, rem)
+def decToBinary(dNum, bRunningTotal):
+    quotient, rem = divmod(dNum, 2)
+    #print(quotient, rem)
 
     if rem == 0:
-        runningTotal = '0' + runningTotal
+        bRunningTotal.insert(0, '0')
     elif rem > 0:
-        runningTotal = '1' + runningTotal
-    print('running total = ' + runningTotal)
+        bRunningTotal.insert(0, '1')
+    print('running total = ' + ''.join(bRunningTotal))
 
     if quotient == 0:
-        return runningTotal
+        print('binary conversion:')
+        print(''.join(bRunningTotal))
+        return bRunningTotal
     else:
-        binaryConv(quotient, runningTotal)
+        decToBinary(quotient, bRunningTotal)
 
 
-num = pyip.inputNum()
-runningTotal = ''
-output = binaryConv(num, runningTotal)
+
+def binaryToDec(bNum, dRunningTotal):
+    bNum.reverse()
+    for i, v in enumerate(bNum):
+        print(i, v, end=' ')
+        print(int(v)*2**i)
+        dRunningTotal.append(int(v)*2**i)
+    x = sum(dRunningTotal)
+    print('decimal conversion:\n' + str(x))
+    return x
+
+
+bRunningTotal = []
+dRunningTotal = []
+
+dNum = pyip.inputInt('enter a decimal number to convert to binary: ')
+b = decToBinary(dNum, bRunningTotal)
+
+bNum = pyip.inputInt('enter a binary number to convert to decimal: ')
+d = binaryToDec(list(str(bNum)), dRunningTotal) # use list representation of bNum as parameter 
 
 
 
